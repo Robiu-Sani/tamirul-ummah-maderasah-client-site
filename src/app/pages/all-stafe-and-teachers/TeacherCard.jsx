62da838af339c0752b7692475311fc70a141a06c
Robiu-Sani <hafazrobiussani@gmail.com>
2025-02-10 11:32:28 +0600

    updateing is done

--- src/app/pages/all-stafe-and-teachers/TeacherCard.jsx
+++ src/app/pages/all-stafe-and-teachers/TeacherCard.jsx
@@ -4,22 +4,28 @@ export default function TeacherCard({ teacher }) {
   // Ensure that teacher properties are defined and valid
   const { teacherImage, teacherName } = teacher || {};
 
-  if (!teacherImage || !teacherName) {
-    return null; // Avoid rendering invalid data
-  }
+  // if (!teacherImage || !teacherName) {
+  //   return null; // Avoid rendering invalid data
+  // }
 
   return (
     <div className="w-full flex flex-col justify-between items-center border rounded-md bg-white shadow-md border-green-100 p-2">
       {/* Render teacher image */}
       <Image
-        src={teacherImage}
-        alt={teacherName}
+        src={
+          teacherImage
+            ? teacherImage
+            : "https://www.pngkey.com/png/detail/115-1150152_default-profile-picture-avatar-png-green.png"
+        }
+        alt={teacherName ? teacherName : "teacher of tamirul ummah maderasah"}
         width={250}
         height={300}
         className="rounded-md"
       />
       {/* Render teacher name */}
-      <div className="mt-2 text-center font-semibold">{teacherName}</div>
+      <div className="mt-2 text-center font-semibold">
+        {teacherName ? teacherName : null}
+      </div>
       <div className="w-full flex flex-col justify-center items-center">
         <small className="text-center">{teacher?.subject}</small>
         <small className="text-center">Mobile : {teacher?.phone}</small>
